441a854b0fe0c6e58362ca714b459241eedb9a9b
Urazof <2778665@gmail.com>
2021-03-15 22:30:36 +0200

    feat: solve brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let arrayString = str.split('');
+    let stack = [];
+    let bracketsConf = Object.fromEntries(bracketsConfig);
+    console.log(bracketsConf)
+    
+    for (let i = 0; i < arrayString.length; i++) {
+        if (stack[stack.length - 1] === arrayString[i]) {
+            stack.pop();
+        } else if (bracketsConf[arrayString[i]]) {
+            stack.push(bracketsConf[arrayString[i]]);
+        } else {
+            return false;
+        }
+    }
+    return stack.length == 0;
 }
+
